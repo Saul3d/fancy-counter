f947e3344ab5c502e51dbbcb7ca6a50d9768a1c6
Saul Solano <knockoutorama@gmail.com>
2024-12-23 18:37:58 -0600

    Removed React import

--- src/Components/Sum.tsx
+++ src/Components/Sum.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 type props = {
   num1: number;
   num2: number;
@@ -7,10 +5,5 @@ type props = {
 
 export default function Sum({ num1, num2 }: props) {
   const total = num1 + num2;
-  return (
-    <>
-      <h1>This is a component used to test jest</h1>
-      <p>The total is {total}</p>
-    </>
-  );
+  return total;
 }
